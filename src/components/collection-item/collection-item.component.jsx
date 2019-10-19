465ff7310621386c9903144c09e65dcff57052a5
Jonas <jonas.borges@dbccompany.com.br>
2019-10-19 15:03:33 -0300

    feat: criação de função para agrupar items no carrinho e contar a quantitade

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -21,7 +21,7 @@ const CollectionItem = ({ item, addItem }) => {
             <span className='name'>{ name }</span>
             <span className='price'>{ price }</span>
         </div>
-        <CustomButton 
+        <CustomButton
             onClick={() => addItem(item)} 
             inverted
         > 
